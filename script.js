24c72c4982afc9a11a859bd974cd6ac517328f7a
Kuang Keng Kuek Ser <kuangkeng@gmail.com>
2015-05-28 17:24:26 -0400

    files updated

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-$("#box01").flip();
-$("#box02").flip();
-$("#box03").flip();
-$("#box04").flip();
-$("#box05").flip();
-$("#box06").flip();
-$("#box07").flip();
-$("#box08").flip();
-$("#box09").flip();
-$("#box10").flip();
-$("#box11").flip();
-$("#box12").flip();
+$("#box01,#box02,#box03,#box04,#box05,#box06,#box07,#box08,#box09,#box10,#box11,#box12").flip();
